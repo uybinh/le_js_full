8b0ef5b2717df78b3421745e6d0ce0260693ee80
Uy Binh <uybinh@gmail.com>
2018-06-04 05:41:17 +0700

    Add click event to thumbnail image
    Click to update image's src, image's title and description.

---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImageTitle = document.querySelector("#gallery-info :first-child");
+  let mainImageDescription = document.querySelector("#gallery-info :last-child");
+
+  thumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener("click", function(event){
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageTitle = thumbnail.dataset.title;
+      let newImageDescription = thumbnail.dataset.description;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImageTitle.textContent = newImageTitle;
+      mainImageDescription.textContent = newImageDescription;
+    });
+  });
+}
+
